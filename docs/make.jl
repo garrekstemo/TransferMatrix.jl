23ade559b15e98847100d77142fad472dbe28f70
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2022-08-30 21:06:29 +0900

    Make pages

--- docs/make.jl
+++ docs/make.jl
@@ -5,4 +5,10 @@ using Documenter, TransferMatrix
 makedocs(
     sitename = "TransferMatrix.jl",
     modules = [TransferMatrix],
+    pages = [
+        "Introduction" => "index.md",
+        "Tutorial" => "tutorial.md",
+        "API Reference" => "reference.md",
+        "References" => "bibliography.md"
+    ]
 )
