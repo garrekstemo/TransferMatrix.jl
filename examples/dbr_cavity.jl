e99310689c7bb3c58bd02c45bcc56d75094688a9
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-07-08 16:44:29 +0900

    Add refractive index profile to dbr-mirror cavity example

--- examples/dbr_cavity.jl
+++ examples/dbr_cavity.jl
@@ -9,7 +9,6 @@ n_tio2 = RefractiveMaterial("main", "TiO2", "Sarkar")
 n_sio2 = RefractiveMaterial("main", "SiO2", "Rodriguez-de_Marcos")
 
 λ_0 = 1.0   # μm
-nperiods = 6
 t_tio2 = λ_0 / (4 * n_tio2(λ_0))
 t_sio2 = λ_0 / (4 * n_sio2(λ_0))
 t_middle = λ_0 / 2
@@ -18,9 +17,10 @@ air = Layer(n_air, t_middle)
 tio2 = Layer(n_tio2, t_tio2)
 sio2 = Layer(n_sio2, t_sio2)
 dbr_unit = [tio2, sio2]
+nperiods = 6
 layers = [air, repeat(dbr_unit, nperiods)..., air, repeat(reverse(dbr_unit), nperiods)..., air];
 
-λs = 0.8:0.001:1.2
+λs = 0.85:0.001:1.2
 νs = 10^4 ./ λs
 Tpp = Float64[]
 Tss = Float64[]
@@ -37,23 +37,39 @@ end
 
 λ = 1.0
 field = electric_field(λ_0, layers)
+ns = [real(layer.dispersion(λ_0)) for layer in layers]
+bounds = [-layers[1].thickness, field.boundaries..., field.boundaries[end] + layers[end].thickness]
+pushfirst!(ns, ns[1])  # Add the refractive index of the first layer for negative z
 
 
-f = Figure(size = (450, 600))
+fig = Figure(size = (450, 800))
 DataInspector()
 
-ax1 = Axis(f[1, 1], title = "ZnS / MgF₂ quarter-wave stack with 3 layers", xlabel = "Wavelength (μm)", ylabel = "Transmittance / Reflectance",
-            yticks = LinearTicks(5), 
-            xticks = LinearTicks(10))
+Label(fig[0, :], "ZnS / MgF₂ quarter-wave stack with 3 layers", fontsize = 18)
 
-lines!(λs, Tpp, label = "T")
-lines!(λs, Rpp, label = "R")
-axislegend(ax1, position = :rc)
+ax1 = Axis(fig[1, 1], xlabel = "Wavenumber (cm⁻¹)", ylabel = "Transmittance / Reflectance",
+            yticks = LinearTicks(5),
+            xticks = LinearTicks(5),
+)
+lines!(νs, Tpp, label = "T")
+lines!(νs, Rpp, label = "R")
 
-ax2 = Axis(f[2, 1], title = "Electric Field at λ = $(Int(λ * 1e3)) nm", xlabel = "z position (nm)", ylabel = "Field intensity (a.u.)")
 
-lines!(field.z .* 1e3, real(field.p[1, :]))
-vlines!(field.boundaries[1], color = :black, linestyle = :dash)
-vlines!(field.boundaries[end] .* 1e3, color = :black, linestyle = :dash)
+# Electric field profile
+ax2 = Axis(fig[2, 1], xlabel = "z position (nm)", ylabel = "Electric field")
 
-f
+lines!(field.z, real(field.p[1, :]))
+text!(-1, 5.2, text="λ = $(Int(λ)) μm")
+
+# Refractive index profile
+ax3 = Axis(fig[3, 1], xlabel = "z position (μm)", ylabel = "Refractive index",
+    xticks = 0:5,
+    yticks = round.(unique(ns), digits=2),
+)
+stairs!(bounds, ns, color = :gray40)
+
+axislegend(ax1)
+linkxaxes!(ax2, ax3)
+rowgap!(fig.layout, 3, 0.0)
+hidexdecorations!(ax2, grid = false)
+fig
